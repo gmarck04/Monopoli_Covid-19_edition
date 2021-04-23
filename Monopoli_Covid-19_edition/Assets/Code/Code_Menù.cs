964f3175373d37407a2c92a6619732d447884006
Giovanni <gmarck04@gmail.com>
2021-04-23 10:26:20 +0200

    Aggiunte funzioni a tutti i pulsanti

--- Monopoli_Covid-19_edition/Assets/Code/Code_Menù.cs
+++ Monopoli_Covid-19_edition/Assets/Code/Code_Menù.cs
@@ -7,29 +7,97 @@ public class Code_Menù : MonoBehaviour
 {
     private static int Quit = 0;
     private static int Back_int = 0;
+    private static int Guida_int = 0;
+    private static int Giocatori = 0;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+        Back_int++;
     }
-    public void Settings_Menù_1()
+    public void Settings()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(5);
+        Back_int++;
+    }
+    public void Guida()
+    {
+        SceneManager.LoadScene(7);
+        Guida_int++;
+        Back_int++;
     }
     public void QuitGame_Menù_1()
     {
+        SceneManager.LoadScene(6);
+        Quit++;
+    }
+    public void CPU_vs_P1()
+    {
+        Giocatori++;
         SceneManager.LoadScene(3);
+        Back_int=+3;
+    }
+    public void Multiplayers()
+    {        
+        SceneManager.LoadScene(2);
+        Back_int++;
+    }
+    public void Giocatore_2()
+    {
+        Giocatori += 2;
+        SceneManager.LoadScene(3);
+        Back_int++;
+    }
+    public void Giocatore_3()
+    {
+        Giocatori += 3;
+        SceneManager.LoadScene(3);
+        Back_int++;
+    }
+    public void Giocatore_4()
+    {
+        Giocatori += 4;
+        SceneManager.LoadScene(3);
+        Back_int++;
+    }
+    private void Giocatori_if()
+    {
+        if (Giocatori == 0)
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+    public void Mascherina()
+    {
+        Giocatori--;
+        Giocatori_if();
+    }
+    public void Siringa()
+    {
+        Giocatori--;
+        Giocatori_if();
+    }
+    public void Vaccino()
+    {
+        Giocatori--;
+        Giocatori_if();
+    }
+    public void Amuchina()
+    {
+        Giocatori--;
+        Giocatori_if();
     }
     public void SÌ()
     {
         Application.Quit();
-        Debug.Log("OK");
     }
     public void NO()
     {
+        Quit--;
         SceneManager.LoadScene(Quit);
     }
     public void Back()
     {
-        SceneManager.LoadScene(Back_int);
+        Back_int--;
+        SceneManager.LoadScene(Back_int);        
     }
 }
